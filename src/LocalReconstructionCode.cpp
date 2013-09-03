49c8a300542be45684f03e8616d4e5c7aa6886a2
Shuai YUAN <yszheda@gmail.com>
2013-09-03 20:57:04 +0800

    add LocalReconstructionCode class

---
+++ src/LocalReconstructionCode.cpp
@@ -0,0 +1,114 @@
+// =====================================================================================
+// 
+//       Filename:  LocalReconstructionCode.cpp
+// 
+//    Description:  Local Reconstruction Code (LRC) used by MS Azure
+// 
+//        Version:  1.0
+//        Created:  2013/9/3 ¤U¤È 06:30:34
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Shuai YUAN (galoisplusplus), yszheda AT gmail DOT com
+//        Company:  
+// 
+// =====================================================================================
+
+#include <LocalReconstructionCode.hpp>
+
+//--------------------------------------------------------------------------------------
+//       Class:  LocalReconstructionCode
+//      Method:  LocalReconstructionCode
+// Description:  constructor
+//--------------------------------------------------------------------------------------
+LocalReconstructionCode::LocalReconstructionCode ( int in_chunk_num, int group_num, int global_parities_num ): 
+		ErasureCode(in_chunk_num, in_chunk_num+group_num+global_parities_num), group_num(group_num), global_parities_num(global_parities_num)
+{
+}  // -----  end of method LocalReconstructionCode::LocalReconstructionCode  (constructor)  -----
+
+//--------------------------------------------------------------------------------------
+//       Class:  LocalReconstructionCode
+//      Method:  LocalReconstructionCode
+// Description:  constructor
+//--------------------------------------------------------------------------------------
+LocalReconstructionCode::LocalReconstructionCode ( int in_chunk_num, int group_num, int global_parities_num, vector< vector<GaloisFieldValue<LRC_gf_width> > > ): 
+		ErasureCode(in_chunk_num, in_chunk_num+group_num+global_parities_num, encoding_matrix), group_num(group_num), global_parities_num(global_parities_num)
+{
+}  // -----  end of method LocalReconstructionCode::LocalReconstructionCode  (constructor)  -----
+
+//--------------------------------------------------------------------------------------
+//       Class:  LocalReconstructionCode
+//      Method:  ~LocalReconstructionCode
+// Description:  destructor
+//--------------------------------------------------------------------------------------
+LocalReconstructionCode::~LocalReconstructionCode ()
+{
+}  // -----  end of method LocalReconstructionCode::~LocalReconstructionCode  (destructor)  -----
+
+//--------------------------------------------------------------------------------------
+//       Class:  LocalReconstructionCode
+//      Method:  update_failure_cnt
+// Description:  
+//--------------------------------------------------------------------------------------
+void LocalReconstructionCode::update_failure_cnt( long failure_state )
+{		
+		for (auto i = 0; i < out_chunk_num; i++) {
+				if (failure_state & 0x01 == 1) {
+						total_cnt ++;
+						if (i < global_parities_num) {
+								failure_cnt.global_parities_cnt ++;
+						} else if (i < global_parities_num + group_num) {
+								failure_cnt.local_parities_cnt[i - global_parities_num] = 1;
+						} else {
+								failure_cnt.native_chunks_cnt[i - global_parities_num - group_num] ++;
+						}
+				}
+		}
+}
+
+//--------------------------------------------------------------------------------------
+//       Class:  LocalReconstructionCode
+//      Method:  check_recoverable
+// Description:  
+//--------------------------------------------------------------------------------------
+bool LocalReconstructionCode::check_recoverable( long failure_state )
+{
+		update_failure_cnt(failure_state);
+		if (failure_cnt.total_cnt <= global_parities_num + 1) {
+				return true;
+		} else if (failure_cnt.total_cnt == global_parities_num + 2) {
+		} else {
+				return false;
+		}
+}
+
+//--------------------------------------------------------------------------------------
+//       Class:  LocalReconstructionCode
+//      Method:  get_min_cost
+// Description:  
+//--------------------------------------------------------------------------------------
+int LocalReconstructionCode::get_min_cost( long failure_state )
+{
+		int min_cost = 0;
+		update_failure_cnt(failure_state);
+		if (failure_cnt.global_parities_cnt > 0) {
+				min_cost = in_chunk_num;
+		} else {
+				bool is_local_recovable = true;
+				int failed_group = 0;
+				for (auto i = 0; i < group_num; i++) {
+						if (native_chunks_cnt[group_num] + local_parities_cnt[group_num] > 1) {
+								is_local_recovable = false;
+								min_cost = in_chunk_num;
+						}
+						if (native_chunks_cnt[group_num] + local_parities_cnt[group_num] > 0) {
+								failed_group ++;
+						}
+
+				}
+				if (is_local_recovable == true) {
+						min_cost = failed_group * (in_chunk_num / group_num);
+				}
+		}
+		return min_cost;
+}
---
+++ src/LocalReconstructionCode.hpp
@@ -0,0 +1,89 @@
+// =====================================================================================
+// 
+//       Filename:  LocalReconstructionCode.hpp
+// 
+//    Description:  Local Reconstruction Code (LRC) used by MS Azure
+// 
+//        Version:  1.0
+//        Created:  2013/9/2 ¤U¤È 09:26:43
+//       Revision:  none
+//       Compiler:  g++
+// 
+//         Author:  Shuai YUAN (galoisplusplus), yszheda AT gmail DOT com
+//        Company:  
+// 
+// =====================================================================================
+
+#ifndef  LOCALRECONSTRUCTIONCODE_H
+#define  LOCALRECONSTRUCTIONCODE_H
+
+#include <iostream>
+#include <vector>
+#include "GaloisFieldValue.hpp"
+using namespace std;
+
+const unsigned int LRC_gf_width = 4;
+// =====================================================================================
+//        Class:  LocalReconstructionCode
+//  Description:  
+// =====================================================================================
+class LocalReconstructionCode: public ErasureCode<LRC_gf_width>
+{
+		public:
+				// ====================  LIFECYCLE     =======================================
+//				LocalReconstructionCode ();                             // constructor
+				LocalReconstructionCode ( int in_chunk_num, int group_num, int global_parities_num );
+				LocalReconstructionCode ( int in_chunk_num, int group_num, int global_parities_num, vector< vector<GaloisFieldValue<LRC_gf_width> > >& encoding_matrix );
+				~LocalReconstructionCode ();                            // destructor
+
+				// ====================  ACCESSORS     =======================================
+				bool check_recoverable ( long failure_state );
+				int get_min_cost ( long failure_state );
+
+				//--------------------------------------------------------------------------------------
+				//       Class:  LocalReconstructionCode
+				//      Method:  get_encoding_matrix
+				//--------------------------------------------------------------------------------------
+				inline vector< vector< GaloisFieldValue<LRC_gf_width> > >
+						LocalReconstructionCode::get_encoding_matrix (  ) const
+						{
+								return encoding_matrix;
+						}		// -----  end of method LocalReconstructionCode::get_encoding_matrix  -----
+
+				//--------------------------------------------------------------------------------------
+				//       Class:  LocalReconstructionCode
+				//      Method:  set_encoding_matrix
+				//--------------------------------------------------------------------------------------
+				inline void
+						LocalReconstructionCode::set_encoding_matrix ( vector< vector< GaloisFieldValue<LRC_gf_width> > > value )
+						{
+								encoding_matrix	= value;
+								return ;
+						}		// -----  end of method LocalReconstructionCode::set_encoding_matrix  -----
+				
+				// ====================  MUTATORS      =======================================
+
+				// ====================  OPERATORS     =======================================
+
+		protected:
+				// ====================  DATA MEMBERS  =======================================
+
+		private:
+				LocalReconstructionCode ( const LocalReconstructionCode &other );   // copy constructor
+				LocalReconstructionCode ( LocalReconstructionCode &&other );   // move constructor
+				LocalReconstructionCode& operator = ( const LocalReconstructionCode &other ); // copy assignment operator
+				LocalReconstructionCode& operator = ( LocalReconstructionCode &&other ); // move assignment operator
+				void update_failure_cnt( long failure_state );
+				// ====================  DATA MEMBERS  =======================================
+				int group_num;
+				int global_parities_num;
+				struct counter {
+						int total_cnt = 0;
+						int global_parities_cnt = 0;
+						int local_parities_cnt[group_num] = {0};
+						int native_chunks_cnt[group_num] = {0};
+				} failure_cnt;				// ----------  end of struct counter ----------
+
+}; // -----  end of class LocalReconstructionCode  -----
+
+#endif   // ----- #ifndef LOCALRECONSTRUCTIONCODE_H  -----
